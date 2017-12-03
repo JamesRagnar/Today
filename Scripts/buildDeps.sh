8495478a4ed3e1f9ea9964ace5dd1de108139e1f
JamesRagnar <2214827+JamesRagnar@users.noreply.github.com>
2017-12-03 10:30:46 -0500

    Skip carthage build if satisfied

--- Scripts/buildDeps.sh
+++ Scripts/buildDeps.sh
@@ -1,2 +1,8 @@
-bundle exec carthage update --platform iOS
+#!/bin/sh
 
+if ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
+    bundle exec carthage update --platform iOS
+    cp Cartfile.resolved Carthage
+fi
+
+echo "Carthage Dependencies Satisfied"
